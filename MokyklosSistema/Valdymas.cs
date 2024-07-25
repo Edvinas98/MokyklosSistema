0c47bcab70c919bb151be01835602a70fc480244
Edvinas98 <pocius07@gmail.com>
2024-07-25 09:44:54 +0300

    Add project files.

---
+++ MokyklosSistema/Valdymas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MokyklosSistema
+{
+    internal class Valdymas
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
